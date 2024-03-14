632b8f536a280f29b72bf0d976a094deded94e4b
yuusakai <sakaiyu24@gmail.com>
2024-03-14 13:37:41 +0000

    add css file

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   get 'chat_room', to: 'chat_room#index', as: 'chat_room'
+  get 'share_room', to: 'share_room#index', as: 'share_room'
 
 end
