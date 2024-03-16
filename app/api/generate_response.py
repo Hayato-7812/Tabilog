9e780a089c13589112a0725e22e1c0f9adbf5d83
Hayato Shimizu <haya12to17@gmail.com>
2024-03-16 05:17:33 +0000

    プロンプトの変更

--- app/api/generate_response.py
+++ app/api/generate_response.py
@@ -8,10 +8,5 @@ load_dotenv()
 # 環境変数に GOOGLE_API_KEY が設定されていれば不要
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
-print(GOOGLE_API_KEY)
 
-# 生きる意味を聞いてみる
 gemini_pro = genai.GenerativeModel("gemini-pro")
-prompt = "What is the meaning of life?"
-response = gemini_pro.generate_content(prompt)
-print(response.text)
--- app/api/generative_ai_handler.rb
+++ app/api/generative_ai_handler.rb
@@ -1,5 +1,7 @@
 require 'pycall'
+require 'dotenv/load'
 
+# GenerativeAiHandlerクラスの定義
 class GenerativeAiHandler
   def initialize(model_name = "gemini-pro")
     @genai = PyCall.import_module('google.generativeai')
@@ -14,3 +16,16 @@ class GenerativeAiHandler
     response.text
   end
 end
+
+# GenerativeAiHandlerのインスタンスを作成
+ai_handler = GenerativeAiHandler.new
+
+# 任意のプロンプトを指定してgenerate_contentメソッドを呼び出す
+prompt = "You are special travel adviser. response to japanese.
+I am planning a two-night, three-day trip to Kyoto from Tokyo with a budget of up to 50,000 yen. 
+I would like to see traditional buildings in Kyoto as much as possible."
+
+generated_text = ai_handler.generate_content(prompt)
+
+# 生成されたテキストを表示
+puts generated_text
