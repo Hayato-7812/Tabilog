cb5401491b27d702f30b685891da04e4d35f1fd3
Hayato Shimizu <haya12to17@gmail.com>
2024-03-22 12:47:40 +0000

    実装1

--- app/controllers/chat_room_controller.rb
+++ app/controllers/chat_room_controller.rb
@@ -1,4 +1,32 @@
 class ChatRoomController < ApplicationController
   def index
   end
+
+  def create
+    # ダミーデータではなく実際のリクエストパラメータを利用する
+    plan = create_travelplan(chat_room_params)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: plan }
+    end
+  end
+
+  private
+
+  # chat_room_params メソッドでフォームからのデータを取得する
+  def chat_room_params
+    params.require(:chat_room).permit(:departure, :budget, :nights, :other)
+  end
+
+  def create_travelplan(json)
+    # 実際の処理をここに実装する
+    # この例ではダミーデータを返す
+    result = {
+      "status": "success",
+      "message": "Travel plan created successfully!",
+      "input": json
+    }
+    return result
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   get 'chat_room', to: 'chat_room#index', as: 'chat_room'
-  get 'share_room', to: 'share_room#index', as: 'share_room'
+  post 'chat_room', to: 'chat_room#create'
 
+  get 'share_room', to: 'share_room#index', as: 'share_room'
 end
