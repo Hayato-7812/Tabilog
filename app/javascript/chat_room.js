810b2650c058213f91f5b73cfe53c6074ca6d4f8
Masaya7707 <sekimasa0707@gmail.com>
2024-04-09 13:42:08 +0000

    Implement #21 feature: チャット画面のフロントエンド

--- app/javascript/chat_room.js
+++ app/javascript/chat_room.js
@@ -1,27 +1,50 @@
 $(function() {
-    // ページが読み込まれたときに実行されるコード
-    $('#chat_departure').val('Hokkaido'); // #chat_departureの値を'New Value'に変更する
-    $('#chat_destination').val('Sapporo');
-    $('#chat_budget').val('50000');
-    $('#chat_nights').val('2');
-    $('#chat_other').val('Sightseeing');
-
     talk_num = 0;
     $('.btn').on('click', function() {
-        if (talk_num == 0){
-            $('#talk_departure .chat-hukidashi-r').html($('.input-box').val());
-            $('#talk_departure').removeClass('hidden');
-            $('#question_destination').removeClass('hidden');
-            $('.input-box').val('')
+        if ($('.input-box').val() == ''){
+            alert('メッセージを入力してください');
+        }
+        else {
+            if (talk_num == 0){
+                $('#talk_departure .chat-hukidashi-r').html($('.input-box').val());
+                $('#chat_departure').val($('.input-box').val()); 
+                $('#talk_departure').removeClass('hidden');
+                $('#question_destination').removeClass('hidden');
+            }
+            if (talk_num == 1){
+                $('#talk_destination .chat-hukidashi-r').html($('.input-box').val());
+                $('#chat_destination').val($('.input-box').val());
+                $('#talk_destination').removeClass('hidden');
+                $('#question_budget').removeClass('hidden');
+            }
+            if (talk_num == 2){
+                if ($('.input-box').val().match(/[^0-9]/)) {
+                    alert('半角数字で入力してください');
+                    talk_num-=1
+                }
+                else {
+                    $('#talk_budget .chat-hukidashi-r').html($('.input-box').val());
+                    $('#chat_budget').val($('.input-box').val());
+                    $('#talk_budget').removeClass('hidden');
+                    $('#question_nights').removeClass('hidden');
+                }
+            }
+            if (talk_num == 3){
+                $('#talk_nights .chat-hukidashi-r').html($('.input-box').val());
+                $('#chat_nights').val($('.input-box').val());
+                $('#talk_nights').removeClass('hidden');
+                $('#question_other').removeClass('hidden');
+            }
+            if (talk_num == 4){
+                $('#talk_other .chat-hukidashi-r').html($('.input-box').val());
+                $('#chat_other').val($('.input-box').val());
+                $('#talk_other').removeClass('hidden');
+                $('#generate_plan').removeClass('hidden');
+            }
+
+            $('.input-box').val('');
             talk_num+=1;
         }
-        // if (talk_num == 1){
-        //     $('#talk_departure .chat-hukidashi-r').html($('.input-box').val());
-        //     $('#talk_departure').removeClass('hidden');
-        //     $('#question_destination').removeClass('hidden');
-        //     talk_num+=1;
-        // }
     });
-    // $('#talk_departure').removeClass('hidden');
-    // $('#question_destination').removeClass('hidden');
+
 });
